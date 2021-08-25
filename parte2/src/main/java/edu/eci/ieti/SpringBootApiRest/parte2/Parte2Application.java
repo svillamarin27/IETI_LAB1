a791f71b9124a5f8f71a3a4e0ff04459b5350b0b
svillamarin27 <sebastian.villamarin@mail.escuelaing.edu.co>
2021-08-25 11:10:53 -0500

    parte 2 ini

---
+++ parte2/src/main/java/edu/eci/ieti/SpringBootApiRest/parte2/Parte2Application.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.SpringBootApiRest.parte2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parte2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parte2Application.class, args);
+	}
+
+}
---
+++ parte2/src/test/java/edu/eci/ieti/SpringBootApiRest/parte2/Parte2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.SpringBootApiRest.parte2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parte2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
